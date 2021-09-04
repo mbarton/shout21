84b4657cf2d3f91e9513320cf6e4bd7a15f0908e
Michael Barton <mbarton@users.noreply.github.com>
2021-09-04 19:17:20 +0000

    Fix HMR in Codespaces

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
 import preprocess from 'svelte-preprocess';
 
+const codespaceName = process.env['CODESPACE_NAME'];
+const hmrPort = 24678;
+
+const hmrRemoteHost = codespaceName ? `${codespaceName}-${hmrPort}.githubpreview.dev` : 'localhost';
+const hmrRemotePort = codespaceName ? 443 : hmrPort;
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,8 +14,17 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+		vite: {
+			server: {
+				hmr: {
+					host: hmrRemoteHost,
+					port: hmrPort,
+					clientPort: hmrRemotePort
+				}
+			}
+		}
+	},
 };
 
 export default config;
