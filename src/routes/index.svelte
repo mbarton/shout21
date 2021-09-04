88f839be7edbd62517cd79af97082f6955798ce7
Michael Barton <mbarton@users.noreply.github.com>
2021-09-04 16:44:01 +0000

    SvelteKit and bulma starter

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+    // TODO MRB: currently causes a flash of unstyled content but only in dev
+    // Have temporarily replaced it with a CDN-hosted build in app.html
+    // https://github.com/sveltejs/kit/issues/915 
+    //import "bulma";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
